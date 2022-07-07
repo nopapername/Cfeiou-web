42371776ac71d802e1c06814e40daacc344e4635
oyoung <1019825864@qq.com>
2022-07-07 18:51:38 +0800

    update: init config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,20 @@
 module.exports = {
-  content: [
-    './src/pages/**/*.tsx',
-    './src/components/**.tsx',
-    './src/layouts/**.tsx',
-  ],
-}
+  purge: {
+    content: [
+      './src/**/*.ts',
+      './src/**/*.tsx',
+      './src/**/*.less',
+      './src/**/*.css',
+      './index.html',
+    ],
+    options: {
+      safelist: ['h-32', 'h-64'],
+    },
+  },
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+};
