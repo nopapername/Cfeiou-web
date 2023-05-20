93e210eab2b8ff2c6710e21390014cf4dfdeeeeb
oyoung <1019825864@qq.com>
2023-05-20 13:48:14 +0800

    fix: update page

--- src/pages/home/home-content/design/index.tsx
+++ src/pages/home/home-content/design/index.tsx
@@ -89,7 +89,7 @@ export default function DesignContent() {
             <div className={classnames(styles['container__design__left-gallery__img'], styles['container__design__left-gallery__img10'])} />
             <div className={styles['container__design__left-gallery__desc']}>
               <div>2021 中旬 / 客厅</div>
-              <div>西雅图</div>
+              <div>成都蜀欣苑</div>
               <div>时尚 简洁</div>
             </div>
           </div>
@@ -97,24 +97,24 @@ export default function DesignContent() {
           <div className={classnames(styles['container__design__left-gallery'], 'scroll-reveal-animation')}>
             <div className={classnames(styles['container__design__left-gallery__img'], styles['container__design__left-gallery__img11'])} />
             <div className={styles['container__design__left-gallery__desc']}>
-              <div>2022 上旬 / 客厅</div>
-              <div>印江山</div>
-              <div>色彩 艺术</div>
+              <div>2023 上旬 / 办公室</div>
+              <div>建造通</div>
+              <div>明亮 规整</div>
             </div>
           </div>
           <div className={classnames(styles['container__design__left-gallery'], 'scroll-reveal-animation')}>
             <div className={classnames(styles['container__design__left-gallery__img'], styles['container__design__left-gallery__img12'])} />
             <div className={styles['container__design__left-gallery__desc']}>
-              <div>Apr 21, 2023 / 客厅</div>
-              <div>龙山景苑</div>
-              <div>清新 明亮</div>
+              <div>2022 设计/ 卧室</div>
+              <div>卧室</div>
+              <div>清新 轻奢</div>
             </div>
           </div>
           <div className={classnames(styles['container__design__left-gallery'], 'scroll-reveal-animation')}>
             <div className={classnames(styles['container__design__left-gallery__img'], styles['container__design__left-gallery__img13'])} />
             <div className={styles['container__design__left-gallery__desc']}>
-              <div>Apr 21, 2023 / 客厅</div>
-              <div>公爵府</div>
+              <div>2021 设计 / 娱乐房</div>
+              <div>儿童房、书房</div>
               <div>精致 现代</div>
             </div>
           </div>
@@ -123,17 +123,17 @@ export default function DesignContent() {
           <div className={classnames(styles['container__design__right-gallery'], 'scroll-reveal-animation')}>
             <div className={classnames(styles['container__design__right-gallery__img'], styles['container__design__right-gallery__img14'])} />
             <div className={styles['container__design__right-gallery__desc']}>
-              <div>Apr 21, 2023 / 阳台</div>
-              <div>长虹天越</div>
-              <div>自然 舒适</div>
+              <div>Apr 21, 2023 / 健身房</div>
+              <div>酷动健身</div>
+              <div>工业 有氧</div>
             </div>
           </div>
           <div className={classnames(styles['container__design__right-gallery'], 'scroll-reveal-animation')}>
             <div className={classnames(styles['container__design__right-gallery__img'], styles['container__design__right-gallery__img15'])} />
             <div className={styles['container__design__right-gallery__desc']}>
-              <div>Apr 21, 2023 / 卧卫</div>
+              <div>Apr 21, 2023 / 阳台</div>
               <div> </div>
-              <div>艺术 温馨<br /></div>
+              <div>优雅 舒适<br /></div>
             </div>
           </div>
           <div className={classnames(styles['container__design__right-gallery'], 'scroll-reveal-animation')}>
@@ -146,7 +146,7 @@ export default function DesignContent() {
           </div>
         </section>
       </div>
-      <div className={styles.design__more__text}>. . . <br /><br />更多案例请联系我们官方微信~</div>
+      <div className={classnames(styles.design__more__text, 'scroll-reveal-animation')}>. . . <br /><br />更多案例请联系我们官方微信~</div>
     </div>
   );
 }
