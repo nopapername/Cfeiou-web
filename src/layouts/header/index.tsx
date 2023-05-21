00ce42012b06cd3c5ee5cf55e4ca9a785e1dbea9
oyoung <1019825864@qq.com>
2023-05-21 14:49:46 +0800

    fix: logo adjust

--- src/layouts/header/index.tsx
+++ src/layouts/header/index.tsx
@@ -1,3 +1,6 @@
+/* eslint-disable no-script-url */
+/* eslint-disable react/jsx-no-script-url */
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import { useModel } from 'umi';
 import logo from '@/assets/header/logo.svg';
 import logoMobile from '@/assets/header/logo_mobile.svg';
@@ -8,7 +11,7 @@ export default function HomePage() {
   const { isMinScreen } = useModel('usePublicState');
   return (
     <section className={styles.header}>
-      <a href="/home" className={styles.header__a}>
+      <a href="javascript:void(0)" onClick={() => scrollToElementById('introduce_with_us')} className={styles.header__a}>
         <img
           className={styles.header__a__logo}
           src={isMinScreen ? logoMobile : logo}
@@ -16,24 +19,12 @@ export default function HomePage() {
         />
       </a>
       <div className={styles.header__menu}>
-        {/* eslint-disable-next-line jsx-a11y/anchor-is-valid,
-            react/jsx-no-script-url,
-            no-script-url
-        */}
         <a href="javascript:void(0)" onClick={() => scrollToElementById('design_with_us')} className={styles['header__menu-item']}>
           {isMinScreen ? '川飞欧' : '川飞欧装修'}
         </a>
-        {/* eslint-disable-next-line jsx-a11y/anchor-is-valid,
-            react/jsx-no-script-url,
-            no-script-url
-        */}
         <a href="javascript:void(0)" onClick={() => scrollToElementById('introduce_with_us')} className={styles['header__menu-item']}>
           简介
         </a>
-        {/* eslint-disable-next-line jsx-a11y/anchor-is-valid,
-            react/jsx-no-script-url,
-            no-script-url
-        */}
         <a href="javascript:void(0)" onClick={() => scrollToElementById('contact_with_us')} className={styles['header__menu-contact']}>
           联系我们
         </a>
