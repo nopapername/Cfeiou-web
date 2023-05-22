f5dbd0e10353d527c24dc3eb6386a771006bddea
oyoung <1019825864@qq.com>
2023-05-22 20:46:26 +0800

    update: add baidu tongji

--- .umirc.ts
+++ .umirc.ts
@@ -32,4 +32,5 @@ export default {
       pathRewrite: { '^/api': '' },
     },
   },
+  headScripts: ['/tongji.js'],
 };
---
+++ public/tongji.js
@@ -0,0 +1,8 @@
+var _hmt = _hmt || [];
+(function () {
+  var hm = document.createElement('script');
+  hm.src = 'https://hm.baidu.com/hm.js?19d74e05a9ea5b67b0017cff767c8eab';
+  var s = document.getElementsByTagName('script')[0];
+  s.parentNode.insertBefore(hm, s);
+})();
+console.log('百度统计添加成功');
