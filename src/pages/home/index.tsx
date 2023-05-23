ffcc29179f20ac685dfaa5a4f95730eb820c6d14
oy <1019825864@qq.com>
2023-05-23 17:40:04 +0800

    fix: image alt优化

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -11,7 +11,7 @@ export default function HomePage() {
       <HomeContent />
       <section className={styles.transform__banner}>
         <div className={styles['transform__banner-wrap']}>
-          <img src="http://cdn.cfeiou.cn/cfeiou-web-image/transform_banner.jpg" alt="banner" />
+          <img src="http://cdn.cfeiou.cn/cfeiou-web-image/transform_banner.jpg" alt="transform_banner" />
           <div className={styles['transform__banner-wrap__slogan']}>
             <span className="scroll-reveal-animation">拥抱，你的美好生活可能性</span>
             <span className="scroll-reveal-animation">Love lights the life</span>
