19168e52f4597821ee96881ba94b0f2a6f63dfad
oy <1019825864@qq.com>
2023-05-23 17:44:08 +0800

    fix: keywords adjust

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -25,7 +25,7 @@ export default function Layout() {
   useMount(() => {
     // 4k
     initRem(isMinScreen, setIsMinScreen);
-    setTitle('绵阳装修', '绵阳装修公司', '川飞欧装饰', '四川飞欧装饰工程有限公司');
+    setTitle('绵阳装修', '绵阳装修工作室', '川飞欧装饰', '四川飞欧装饰工程有限公司');
   });
 
   useEffect(() => {
@@ -44,7 +44,7 @@ export default function Layout() {
   return (
     <>
       <Helmet>
-        <meta name="keywords" content="'绵阳装修', '绵阳装修公司'，绵阳本土装修公司，绵阳室内设计，绵阳商业装修，室内设计，川飞欧装饰， 川飞欧" />
+        <meta name="keywords" content="绵阳装修, 绵阳装修公司，绵阳本土装修公司，绵阳商业装修，川飞欧装饰" />
         <meta name="description" content="绵阳本地装修找川飞欧，在2021年，川飞欧成立于四川绵阳，旨在为客户提供合理的设计方案创造高品质商业或居住空间。川飞欧，与你一起构建「更美好的生活」。" />
         <meta name="description" content="川飞欧公司董事长在成立川飞欧之前，是一家当地大型装修公司的副总经理、总项目经理及股东，从事装修行业近20余年。" />
         <meta name="location" content="province=四川;city=绵阳;coord=104.791281,31.41705" />
